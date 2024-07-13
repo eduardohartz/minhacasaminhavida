7cd0555163b8a45ac37479cac19c4ff6500b6cb7
Breezien <80062347+Breezien@users.noreply.github.com>
2024-07-12 22:29:04 -0300

    more

--- assets/js/documentos.js
+++ assets/js/documentos.js
@@ -5,12 +5,27 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const sucesso = urlParams.get('sucesso');
     const id = urlParams.get('id');
+    const emprego = urlParams.get('emprego');
     const input = this.getElementById('simulacao');
-    
+    const assalariado = this.getElementById('assalariado');
+    const autonomo = this.getElementById('autonomo');
+
     if (id) {
         input.value = id;
-    } else if(sucesso == "" || !sucesso) {
+    } else if (sucesso == "" || !sucesso) {
         //window.location.href = '/?sucesso=0';
+    }
+
+    if (emprego) {
+        if (emprego == 'assalariado') {
+            autonomo.style.visibility = "hidden";
+            autonomo.hidden = true;
+            assalariado.querySelector("input").setAttribute("required", "");
+        } else if (emprego == 'autonomo') {
+            assalariado.style.visibility = "hidden";
+            assalariado.hidden = true;
+            autonomo.querySelector("input").setAttribute("required", "");
+        }
     }
 
     if (sucesso.includes('1')) {
